4d725c433d73f375f0b102cb9d1e6991ca7468a3
DeckerCHAN <DeckerCHAN@gmail.com>
2014-06-11 21:47:04 +0800

    Add initial files

---
+++ src/main/java/com/decker/essentiallib/Plugin.java
@@ -0,0 +1,29 @@
+package com.decker.essentiallib;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Plugin extends org.sipc.se.plugin.PluginImpl
+{
+
+    @Override
+    public void getResponse(HttpServletRequest request, HttpServletResponse response)
+    {
+	
+	
+    }
+
+    @Override
+    public String getUrl()
+    {
+	return null;
+    }
+
+    @Override
+    public boolean onEnable()
+    {
+	
+	return true;
+    }
+
+}

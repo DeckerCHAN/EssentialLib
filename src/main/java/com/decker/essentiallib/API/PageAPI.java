39c5b66f157436a4e5e657a67a40e2ea05c1c8d0
DeckerCHAN <DeckerCHAN@gmail.com>
2014-06-13 16:16:03 +0800

    Add  register page and pre-load js functions to API

--- src/main/java/com/decker/essentiallib/API/PageAPI.java
+++ src/main/java/com/decker/essentiallib/API/PageAPI.java
@@ -1,5 +1,15 @@
 package com.decker.essentiallib.API;
 
-public class PageAPI {
+import com.decker.essentiallib.Page;
+import com.decker.essentiallib.PageTitleIcon;
+import com.decker.essentiallib.Reactor;
 
+public class PageAPI {
+    public static void reigsterPage(String title, String url, PageTitleIcon icon) {
+	Reactor.getReactor().addSidelistItem(title, new Page(title, url, icon));
+    }
+
+    public static void addPreloadJavascript(String url) {
+	Reactor.getReactor().addPreloadJavaScript(url);
+    }
 }

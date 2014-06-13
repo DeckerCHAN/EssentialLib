68ef10a73662c35ac8b1a15096310df30c84360b
DeckerCHAN <DeckerCHAN@gmail.com>
2014-06-13 22:52:42 +0800

    Add page level

--- src/main/java/com/decker/essentiallib/API/PageAPI.java
+++ src/main/java/com/decker/essentiallib/API/PageAPI.java
@@ -5,8 +5,8 @@ import com.decker.essentiallib.PageTitleIcon;
 import com.decker.essentiallib.Reactor;
 
 public class PageAPI {
-    public static void reigsterPage(String title, String url, PageTitleIcon icon) {
-	Reactor.getReactor().addFuelRod(title, new Page(title, url, icon));
+    public static void reigsterPage(String title, String url, PageTitleIcon icon,int pageLevel) {
+	Reactor.getReactor().addFuelRod(title, new Page(title, url, icon,pageLevel));
     }
 
     public static void addPreloadJavascript(String url) {
--- src/main/java/com/decker/essentiallib/Page.java
+++ src/main/java/com/decker/essentiallib/Page.java
@@ -1,10 +1,11 @@
 package com.decker.essentiallib;
 
 public class Page {
-    public Page(String title, String url, PageTitleIcon icon) {
+    public Page(String title, String url, PageTitleIcon icon,int pageLevel) {
 	this.title = title;
 	this.url = url;
 	this.icon = icon;
+	this.pageLevel=pageLevel;
     }
 
     public String getUrl() {
@@ -18,8 +19,10 @@ public class Page {
     public PageTitleIcon getIcon() {
 	return this.icon;
     }
-
+    
+    
     private String url;
     private String title;
     private PageTitleIcon icon;
+    private int pageLevel;
 }

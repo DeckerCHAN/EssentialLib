c17f5b2381aba53d0f47de069a97833e2d6a22ad
DeckerCHAN <DeckerCHAN@gmail.com>
2014-06-17 02:19:18 +0800

    Add new icons

--- src/main/java/com/decker/essentiallib/PageTitleIcon.java
+++ src/main/java/com/decker/essentiallib/PageTitleIcon.java
@@ -2,9 +2,8 @@ package com.decker.essentiallib;
 
 public enum PageTitleIcon {
 
-    HOME("icon-home"),
-    ALIAN_LEFT("alian-left");
-    public String value; 
+    HOME("icon-home"), ALIAN_LEFT("alian-left"), FIRE("fire"), FLASH("flash");
+    public String value;
 
     private PageTitleIcon(String value) {
 	this.value = value;
